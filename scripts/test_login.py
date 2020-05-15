c9a1f62f1ad08334e7854d66dde002662e2f4f45
ruanlanqing <847002448@qq.com>
2020-05-15 17:49:19 +0800

    更新成 断言失败了

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -6,11 +6,8 @@ class TestLogin:
 
     @allure.step(title='测试1')
     def test_login001(self):
-        print('点击放大镜')
-        print('输入文字')
-        print('点击返回')
+		assert 0
 
     @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
     def test_login002(self):
-        print('点击放大镜')
-        print('点击返回')
+		assert 1
