5d1eb09335de4e1389f2d043f28971aa3d3aeff3
ruanlanqing <847002448@qq.com>
2020-05-15 17:57:54 +0800

    修改了格式

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -4,10 +4,10 @@ import pytest
 
 class TestLogin:
 
-    @allure.step(title='测试1')
-    def test_login001(self):
-		assert 0
 
-    @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
+    def test_login001(self):
+        assert 0
+
+
     def test_login002(self):
-		assert 1
+        assert 1
